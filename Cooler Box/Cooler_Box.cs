f8244f1cd103b43deca85ec4f820e5f9ff341e80
Trimpsuz <me@trimpsuz.dev>
2024-02-18 03:19:06 +0200

    Initial commit

---
+++ Cooler Box/Cooler_Box.cs
@@ -0,0 +1,100 @@
+﻿using MSCLoader;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace Cooler_Box
+{
+    public class Cooler_Box : Mod
+    {
+        public override string ID => "Cooler_Box"; // Your (unique) mod ID 
+        public override string Name => "Cooler Box"; // Your mod name
+        public override string Author => "Trimpsuz"; // Name of the Author (your name)
+        public override string Version => "1.0"; // Version
+        public override string Description => "A usable cooler box. Half as good as the fridge."; // Short description of your mod
+
+        public GameObject cooler;
+        public AssetBundle assets;
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+            SetupFunction(Setup.OnSave, Mod_OnSave);
+            SetupFunction(Setup.Update, Mod_Update);
+            SetupFunction(Setup.ModSettings, Mod_Settings);
+        }
+
+        private void Mod_Settings()
+        {
+            // All settings should be created here. 
+            // DO NOT put anything that isn't settings or keybinds in here!
+        }
+
+        private void Mod_OnLoad()
+        {
+            assets = LoadAssets.LoadBundle(Properties.Resources.cooler);
+            cooler = assets.LoadAsset<GameObject>("cooler.prefab");
+            cooler = Object.Instantiate(cooler);
+            assets.Unload(false);
+            cooler.name = "Cooler(Clone)";
+            cooler.layer = LayerMask.NameToLayer("Parts");
+            cooler.tag = "PART";
+            cooler.MakePickable();
+
+            if(File.Exists(Path.Combine(Application.persistentDataPath, "CoolerBox.json")))
+            {
+                using (FileStream stream = File.OpenRead(Path.Combine(Application.persistentDataPath, "CoolerBox.json")))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    using (StreamReader sr = new StreamReader(stream))
+                    using (JsonTextReader reader = new JsonTextReader(sr))
+                    {
+                        Save s = serializer.Deserialize<Save>(reader);
+                        cooler.transform.position = new Vector3(s.x, s.y, s.z);
+                        cooler.transform.rotation = Quaternion.Euler(s.xRot, s.yRot, s.zRot);
+                    }
+                }
+            } else
+            {
+                cooler.transform.position = GameObject.Find("PLAYER").transform.position;
+            }
+        }
+        private void Mod_OnSave()
+        {
+            using (FileStream stream = File.OpenWrite(Path.Combine(Application.persistentDataPath, "CoolerBox.json")))
+            {
+                Save s = new Save(cooler.transform.position, cooler.transform.rotation);
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(stream))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, s);
+                }
+            }
+
+        }
+        private void Mod_Update()
+        {
+            // Update is called once per frame
+        }
+    }
+
+    public class Save
+    {
+        public float x, y, z;
+        public float xRot, yRot, zRot;
+
+        public Save() { }
+        public Save(Vector3 pos, Quaternion rot)
+        {
+            x = pos.x;
+            y = pos.y;
+            z = pos.z;
+
+            xRot = rot.eulerAngles.x;
+            yRot = rot.eulerAngles.y;
+            zRot = rot.eulerAngles.z;
+        }
+    }
+}
