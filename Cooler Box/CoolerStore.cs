8b1b3b885bc8dec6f1bb761d493c755059b08e3c
Trimpsuz <me@trimpsuz.dev>
2024-02-19 05:10:55 +0200

    Implement refunding cooler

--- Cooler Box/CoolerStore.cs
+++ Cooler Box/CoolerStore.cs
@@ -48,30 +48,49 @@ namespace Cooler_Box
             {
                 camera = Camera.main;
             }
-            else if (!orderToPay)
+            else
             {
                 foreach (RaycastHit hit in Physics.RaycastAll(camera.transform.position, camera.transform.forward, 1f))
                 {
                     if (hit.collider.gameObject == gameObject || hit.collider.gameObject == lid.gameObject || hit.collider.gameObject == handle.gameObject)
                     {
-                        mouseOver = true;
-                        guiBuy.Value = true;
-                        guiInteraction.Value = "Cooler Box 50 mk";
-                        if (Input.GetMouseButtonDown(0))
+                        if (orderToPay)
                         {
                             mouseOver = true;
-                            guiBuy.Value = false;
-                            guiInteraction.Value = "";
-                            orderToPay = true;
-                            GetComponent<MeshRenderer>().enabled = false;
-                            lid.GetComponent<MeshRenderer>().enabled = false;
-                            handle.GetComponent<MeshRenderer>().enabled = false;
-                            cashRegister.FsmVariables.GetFsmFloat("PriceTotal").Value += 50f;
-                            cashRegister.SendEvent("PURCHASE");
-                            return;
+                            guiBuy.Value = true;
+                            guiInteraction.Value = "Cooler Box 50 mk";
+                            if (Input.GetMouseButtonDown(1))
+                            {
+                                mouseOver = true;
+                                orderToPay = false;
+                                GetComponent<MeshRenderer>().enabled = true;
+                                lid.GetComponent<MeshRenderer>().enabled = true;
+                                handle.GetComponent<MeshRenderer>().enabled = true;
+                                cashRegister.FsmVariables.GetFsmFloat("PriceTotal").Value -= 50f;
+                                cashRegister.SendEvent("PURCHASE");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            mouseOver = true;
+                            guiBuy.Value = true;
+                            guiInteraction.Value = "Cooler Box 50 mk";
+                            if (Input.GetMouseButtonDown(0))
+                            {
+                                mouseOver = true;
+                                orderToPay = true;
+                                GetComponent<MeshRenderer>().enabled = false;
+                                lid.GetComponent<MeshRenderer>().enabled = false;
+                                handle.GetComponent<MeshRenderer>().enabled = false;
+                                cashRegister.FsmVariables.GetFsmFloat("PriceTotal").Value += 50f;
+                                cashRegister.SendEvent("PURCHASE");
+                                return;
+                            }
                         }
                         break;
-                    } else if (mouseOver)
+                    }
+                    else if (mouseOver)
                     {
                         mouseOver = false;
                         guiBuy.Value = false;
